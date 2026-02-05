5104959e27666eea22a0db664326aa90b0961804
nishanthir2006-creator <nishanthi.r2006@gmail.com>
2026-02-05 10:48:19 +0530

    Add files via upload

---
+++ Exchange.java
@@ -0,0 +1,14 @@
+class Exchange {
+public static void main
+(String[] args) {
+int a = 10;
+int b = 8;
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println
+("Exchange b:"+b);
+System.out.println
+("Exchange a:"+a);
+}
+}
