509d5065b64a3c63214b143cb562bc0d6bcaec04
nishanthir2006-creator <nishanthi.r2006@gmail.com>
2026-02-05 17:31:49 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int rev = 0, temp = n;
+
+        while (n > 0) {
+            rev = rev * 10 + (n % 10);
+            n /= 10;
+        }
+
+        System.out.println(temp == rev ? "Valid" : "Invalid");
+    }
+}
